c2785100e1f9f447654aa945917bfa2d779433a1
Patricio Quezada AsusK501 <patricio.quezada05@gmail.com>
2019-05-07 09:05:33 -0400

    downgrade mysql version

--- public_html/index.php
+++ public_html/index.php
@@ -1,13 +1,14 @@
 <h1>Hello Cloudreach!</h1>
 <h4>Attempting MySQL connection from php...</h4>
 <?php
-$host = 'mysql';
-$user = 'root';
-$pass = 'rootpassword';
-$conn = new mysqli($host, $user, $pass);
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+$mysqli = new MySQLi("mysql","root","rootpassword");
+echo "<pre>";
+print_r($mysqli);
+echo "</pre>";
+
+if ($mysqli->connect_error) {
+    die("Connection failed: " . $mysqli->connect_error);
 } else {
     echo "Connected to MySQL successfully!";
 }
