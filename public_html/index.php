023590969afd8ebb693a7e9392ab9c42721aebcf
Patricio Quezada DEPLANE <patricio.quezada05@gmail.com>
2019-05-07 15:10:58 -0400

    Angus v1.0

--- public_html/index.php
+++ public_html/index.php
@@ -1,16 +1,12 @@
-<h1>Hello Cloudreach!</h1>
+<h1>Docker Setup OK!</h1>
 <h4>Attempting MySQL connection from php...</h4>
 <?php
 
 $mysqli = new MySQLi("mysql","root","rootpassword");
-echo "<pre>";
-print_r($mysqli);
-echo "</pre>";
 
 if ($mysqli->connect_error) {
     die("Connection failed: " . $mysqli->connect_error);
 } else {
     echo "Connected to MySQL successfully!";
 }
-
 ?>
